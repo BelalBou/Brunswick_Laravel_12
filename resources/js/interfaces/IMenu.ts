a8b9979b04eac14a1679787dd59c2b475a6de759
BelalBou <belalbou48@gmail.com>
2025-03-19 14:14:59 +0100

    coorection des interfaces

--- resources/js/interfaces/IMenu.ts
+++ resources/js/interfaces/IMenu.ts
@@ -1,24 +1,37 @@
-import IAllergy from "./IAllergy";
-import ICategory from "./ICategory";
-import IMenuSize from "./IMenuSize";
-import IOrderMenu from "./IOrderMenu";
-import IExtra from "./IExtra";
-import ISupplier from "./ISupplier";
+import IOrderMenu from './IOrderMenu';
+import IExtra from './IExtra';
+import ICategory from './ICategory';
+import IMenuSize from './IMenuSize';
+import ISupplier from './ISupplier';
 
+/**
+ * Interface représentant un menu dans l'application
+ */
 export default interface IMenu {
+  /** Identifiant unique du menu */
   id: number;
+  /** Titre du menu */
   title: string;
-  title_en: string;
-  pricing: string;
+  /** Prix du menu */
+  pricing: number;
+  /** Description du menu */
   description: string;
-  description_en: string;
-  order_menus: IOrderMenu;
+  /** Image du menu */
+  image: string;
+  /** Indique si le menu est actif */
+  is_active: boolean;
+  /** Indique si le menu est disponible */
+  is_available: boolean;
+  /** ID du fournisseur associé */
+  supplier_id: number;
+  /** ID de la catégorie associée */
   category_id: number;
+  /** ID de la taille du menu associée */
   menu_size_id: number;
-  picture: string;
-  Allergy: IAllergy[];
-  Extra: IExtra[];
-  Category: ICategory;
-  MenuSize: IMenuSize;
-  Supplier: ISupplier;
+  /** Relations avec d'autres entités */
+  order_menu: IOrderMenu[];
+  extras: IExtra[];
+  category: ICategory;
+  menu_size: IMenuSize;
+  supplier: ISupplier;
 }
